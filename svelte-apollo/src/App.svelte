d253524c346964bafe6ec4bd683297b4e2fa0e6d
koo5 <kolman.jindrich@gmail.com>
2020-12-13 14:31:08 +0100

    ...f

--- svelte-apollo/src/App.svelte
+++ svelte-apollo/src/App.svelte
@@ -70,5 +70,5 @@
 		<li>figuring out how to svelte right.</li>
 	</ul>
 	<hr>
-	Thanks for you attention! Created by koo &amp; aindilis.
+	Thanks for you attention! Created by koo &amp; aindilis. <a href="https://github.com/koo5/koordinator2000/blob/master/README.md">https://github.com/koo5/koordinator2000/blob/master/README.md</a>
 </section>
