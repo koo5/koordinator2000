cf7a31e4ba3b3c83ac05cec2feb4d53458912265
koo5 <kolman.jindrich@gmail.com>
2021-02-09 22:34:55 +0100

    ..

--- sapper4/sapper/src/routes/index.svelte
+++ sapper4/sapper/src/routes/index.svelte
@@ -24,9 +24,9 @@
 		Your automatically-assigned username is "{$my_user.name}".
 	{/if}
 </p>
-<h1>Where am i?</h1>
+<h2>Where am i?</h2>
 <p>This is a preview of Koordinator. It's like kickstarter, but not for collecting money, it's for doing things.</p><p>On Kickstarter, people support campaigns by pledging money. If enough money is pledged, the campaign author can collect them. Here, you pledge your participation in campaigns, and you choose how many people is enough. For example, you would not boycott a supermarket chain all alone. Here you can create a campaign and suggest a threshold number. Maybe 50000 people? That should make a dent in their profits!</p>
-<h1>It looks broken?</h1>
+<h2>It looks broken?</h2>
 <p>Koordinator is a <a href="https://github.com/koo5/koordinator2000/">work in progress</a>. You can click around, view campaigns,
 	participate, see and confirm notifications, you can swipe left to dismimss and right to participate.. The basic
 	logic is implemented, but mostly it is still just a demo with some fake buttons and no authentication.</p>
