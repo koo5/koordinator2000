804a2e4511b80198e873c549f431bbda3e71e98b
koo5 <kolman.jindrich@gmail.com>
2021-03-21 16:36:16 +0100

    donation button?

--- sapper4/sapper/src/routes/about.svelte
+++ sapper4/sapper/src/routes/about.svelte
@@ -7,31 +7,33 @@
 
 <h1>About fullcracy/koordinator</h1>
 
-<h3>How to help</h3>
+<h3>Talk</h3>
 <a href="https://github.com/koo5/koordinator2000/issues/">github</a><br>
 <a href="https://webchat.freenode.net/#koordinator">IRC</a><br>
 <a href="https://discord.gg/QP9mhdEk">discord</a>.<br>
+<h3>Help</h3>
+<a href="https://www.codetriage.com/koo5/koordinator2000"><img src='https://www.codetriage.com/koo5/koordinator2000/badges/users.svg'/></a>
 
-<h3>How to fund the development</h3>
+<h3>Fund</h3>
 
-<h5>Librapay</h5>
-
+<h5>Liberapay</h5>
 <script src="https://liberapay.com/koo5/widgets/button.js"></script>
 {@html '<script src="https://liberapay.com/koo5/widgets/receiving.js"></script>'}
-
 <a href="https://liberapay.com/koo5/donate">
 <img src="https://img.shields.io/liberapay/receives/koo5.svg?logo=liberapay">
 </a>
 
-<h5>...</h5>
+
+<h5>GitHub</h5>
+<a href="https://github.com/sponsors/koo5">GitHub Sponsors</a>
 
 
-<h3>friend projects</h3>
+<h3>Friend projects</h3>
 
 <a rel=prefetch href="see_also">see_also</a>.
 
 
-<h3>Thank you</h3>
+<h3>Thank You</h3>
 
 Thank you for you attention! Created by koo &amp; aindilis. <a href="https://github.com/koo5/koordinator2000/
 blob/master/README.md">https://github.com/koo5/koordinator2000/blob/master/README.md</a>
