8f3842f1e80aa98004c3d009380709aa1289d884
stoopkid <stoopkid@stoopkid.com>
2021-04-06 23:10:40 -0400

    TabularParticipationsBreakdown: row for suggested threshold

--- sapper4/sapper/src/components/TabularParticipationsBreakdown.svelte
+++ sapper4/sapper/src/components/TabularParticipationsBreakdown.svelte
@@ -1,10 +1,15 @@
 <script>
-	import ToolTipsy from 'cmps/ToolTipsy.svelte';
+	//import ToolTipsy from 'cmps/ToolTipsy.svelte';
 	import {Table} from 'sveltestrap';
-	import {get_status_class,get_tickmark,short_description,long_description} from 'srcs/stuff.js';
-	import {my_user} from 'srcs/my_user.js';
+	//import {get_status_class,get_tickmark,short_description,long_description} from 'srcs/stuff.js';
+	//import {my_user} from 'srcs/my_user.js';
+	import TabularParticipationsRow from 'cmps/TabularParticipationsRow.svelte';
 
 	export let campaign;
+
+	var suggested = campaign.suggested_lowest_threshold;
+	var le = campaign.participations.filter(participation => participation.threshold <= suggested);
+	var gt = campaign.participations.filter(participation => participation.threshold > suggested);
 </script>
 <table responsive>
 	<thead>
@@ -16,29 +21,17 @@
 	</tr>
 	</thead>
 	<tbody>
-	{#each campaign.participations as participation, idx (participation.id)}
-		<tr>
-			<td class={get_status_class(participation)}>{idx + 1}</td>
-			<td><a href="/users/{participation.account.id}">{participation.account.name}</a></td>
-			<td>
-				<ToolTipsy>{participation.threshold}
-					<div slot="tooltip" class="info_tooltip">
-						The number of other users that must start participating, before this user starts.
-						{participation.threshold} other users must start first.
-					</div>
-				</ToolTipsy>
-			</td>
-			<td>
-				<ToolTipsy enabled="{!$my_user.hide_help}">
-				{get_tickmark(participation)} - {short_description(participation)}
-				<div slot="tooltip">
-					<div class="help_tooltip">
-						{long_description(participation)}
-					</div>
-				</div>
-				</ToolTipsy>
-			</td>
-		</tr>
+	{#each le as participation}
+		<TabularParticipationsRow participation={participation}/>
+	{/each}
+	<tr style="font-weight:bold">
+		<td></td>
+		<td>Suggested threshold</td>
+		<td>{suggested}</td>
+		<td></td>
+	</tr>
+	{#each gt as participation}
+		<TabularParticipationsRow participation={participation}/>
 	{/each}
 	</tbody>
 </table>
@@ -50,4 +43,5 @@
 		border-collapse: separate;
 		border-spacing: 1em 0;
 	}
+
 </style>
---
+++ sapper4/sapper/src/components/TabularParticipationsRow.svelte
@@ -0,0 +1,36 @@
+<script>
+  import ToolTipsy from 'cmps/ToolTipsy.svelte';
+  import {get_status_class,get_tickmark,short_description,long_description} from 'srcs/stuff.js';
+  import {my_user} from 'srcs/my_user.js';
+
+
+  export let participation;
+
+  const idx = participation.id;
+</script>
+
+<tr>
+  <td class={get_status_class(participation)}>{idx + 1}</td>
+
+  <td><a href="/users/{participation.account.id}">{participation.account.name}</a></td>
+
+  <td>
+    <ToolTipsy>{participation.threshold}
+      <div slot="tooltip" class="info_tooltip">
+        The number of other users that must start participating, before this user starts.
+        {participation.threshold} other users must start first.
+      </div>
+    </ToolTipsy>
+  </td>
+
+  <td>
+    <ToolTipsy enabled="{!$my_user.hide_help}">
+    {get_tickmark(participation)} - {short_description(participation)}
+    <div slot="tooltip">
+      <div class="help_tooltip">
+        {long_description(participation)}
+      </div>
+    </div>
+    </ToolTipsy>
+  </td>
+</tr>
