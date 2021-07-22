fe5170bcfe4522b98cddf89ded888d947a7e035b
koo5 <kolman.jindrich@gmail.com>
2021-07-22 21:35:06 +0200

    ..

--- sapper4/sapper/src/config.js
+++ sapper4/sapper/src/config.js
@@ -1,11 +1,4 @@
-exports.config = {}
-if (!process.browser)
-{
-	var dotenv = require('dotenv')
-	var myEnv = dotenv.config()
-	var dotenvExpand = require('dotenv-expand')
-	dotenvExpand(myEnv)
-	exports.config =
+exports.config =
 		{
 			GRAPHQL_ENDPOINT:"hasura-3f257037.nhost.app/v1/graphql",
 			PUBLIC_URL:"http://blissful-nobel-7b9749.netlify.app",
