2cfa7ede31c24caa5cf28faa972404bf77dcb827
koo5 <kolman.jindrich@gmail.com>
2021-07-22 21:39:10 +0200

    ..

--- sapper4/sapper/src/config.js
+++ sapper4/sapper/src/config.js
@@ -1,6 +1,5 @@
 exports.config =
-		{
-			GRAPHQL_ENDPOINT:"hasura-3f257037.nhost.app/v1/graphql",
-			PUBLIC_URL:"http://blissful-nobel-7b9749.netlify.app",
-		}
+{
+	GRAPHQL_ENDPOINT:"hasura-3f257037.nhost.app/v1/graphql",
+	PUBLIC_URL:"http://blissful-nobel-7b9749.netlify.app",
 }
