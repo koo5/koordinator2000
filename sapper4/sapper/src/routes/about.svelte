3710eae262841b23181a5aaf8c16f316b49b1e6b
koo5 <kolman.jindrich@gmail.com>
2022-04-14 22:38:52 +0200

    fixed "Talk" links

--- sapper4/sapper/src/routes/about.svelte
+++ sapper4/sapper/src/routes/about.svelte
@@ -7,13 +7,13 @@
 <h3>Contact</h3>
 <div class="content_block">
 
-	<a href="https://github.com/koo5/koordinator2000/issues/">github</a><br>
-	<a href="https://webchat.freenode.net/#koordinator">IRC</a><br>
-	<a href="https://discord.gg/QP9mhdEk">discord</a>.<br>
+	<a href="https://web.libera.chat/#koordinator">IRC</a><br>
+	<a href="https://discord.gg/CQHQ94y4Er">discord</a>.<br>
 
 </div>
 <h3>Help</h3>
 <div class="content_block">
+	<a href="https://github.com/koo5/koordinator2000/">github</a><br>
 	<a href="https://www.codetriage.com/koo5/koordinator2000">
 		<img src='https://www.codetriage.com/koo5/koordinator2000/badges/users.svg' alt='codetriage'/>
 	</a>
@@ -41,7 +41,7 @@
 <h3>Thank You!</h3>
 <div class="content_block">
 
-	Thank You for You attention! Created by koo5 &amp; aindilis &amp; stoopkid &amp; mst &amp; maybe some other people we have forgotten about.. <a href="https://github.com/koo5/koordinator2000/
+	Thank You for Your attention! Created by koo5 &amp; aindilis &amp; stoopkid &amp; mst &amp; maybe some other people we have forgotten about... <a href="https://github.com/koo5/koordinator2000/
 blob/master/README.md">https://github.com/koo5/koordinator2000/blob/master/README.md</a>
 
 </div>
--- sapper4/sapper/src/routes/about2.svelte
+++ sapper4/sapper/src/routes/about2.svelte
@@ -34,34 +34,8 @@
 
 	<h4>Talk</h4>
 
-	<a href="https://github.com/koo5/koordinator2000/issues/">github</a><br>
-	<a href="https://webchat.freenode.net/#koordinator">IRC</a><br>
-	<a href="https://discord.gg/QP9mhdEk">discord</a>.<br>
 
-	<h4>Help</h4>
-	<a href="https://www.codetriage.com/koo5/koordinator2000">
-		<img src='https://www.codetriage.com/koo5/koordinator2000/badges/users.svg' alt='codetriage'/>
-	</a>
-
-	<h4>Fund</h4>
-
-	<h5>Liberapay</h5>
-	<script src="https://liberapay.com/koo5/widgets/button.js"></script>
-	{@html '<script src="https://liberapay.com/koo5/widgets/receiving.js"></script>'}
-	<a href="https://liberapay.com/koo5/donate">
-		<img src="https://img.shields.io/liberapay/receives/koo5.svg?logo=liberapay" alt="liberapay">
-	</a>
-
-
-	<h5>GitHub</h5>
-	<a href="https://github.com/sponsors/koo5">GitHub Sponsors</a>
-
-
-	<h4>Friend projects</h4>
-
-	<a rel=prefetch href="see_also">see_also</a>.
-
-
+	bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla vv
 	<h4>Thank You</h4>
 
 	Thank you for you attention! Created by koo &amp; aindilis. <a href="https://github.com/koo5/koordinator2000/
